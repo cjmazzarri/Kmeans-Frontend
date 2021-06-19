bd9f28a49ce887840990e0dbfffc122e72437f4c
cjmazzarri <cjmazzarri@gmail.com>
2021-06-18 21:38:00 -0500

    ✨Added component for viewing initial data

--- kmeans-app/src/App.vue
+++ kmeans-app/src/App.vue
@@ -1,15 +1,55 @@
 <template>
   <div class="container">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img src="./assets/UPC_logo_transparente.png" style="width: 5%">
+    <p>Programación Concurrente y Distribuida - TA2<br>Dataset: Bonos y pensiones complementarias administradas por la ONP Agosto 2020</p>
+
+    <div class="data">
+      <table style="width: 100%; border: 2px solid black">
+        <tr>
+          <th>Cluster</th> <th>Id_Persona</th> <th>Prestacion</th><th>Tipotra</th><th>Tipoben</th><th>Beneficio</th>          
+        </tr>
+        <tr v-for="(dato, index) in datos" v-bind:key="index">              
+          <td>{{dato.Id_persona}}</td>
+          <td>{{dato.Prestacion}}</td>
+          <td>{{dato.Tipotra}}</td>
+          <td>{{dato.Tipoben}}</td>
+          <td>{{dato.Beneficio}}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import axios from 'axios'
 export default {
   name: 'App',
-  
+  data: () => {
+    return {
+      datos: [],
+      errors: '',
+      indices: []
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:9000/").then(response =>{
+      console.log(JSON.stringify(response.data))
+      this.datos = response.data
+      console.log(this.datos)  
+    }).catch(error =>{
+      console.log(error)
+      this.errors = error
+    })
+
+    axios.get("http://localhost:9000/grupos").then(response =>{
+      console.log(JSON.stringify(response.data))
+      this.indices = response.data
+      console.log(this.indices)
+    }).catch(error =>{
+      console.log(error)
+      this.errors = error
+    })
+  }
 }
 </script>
 
